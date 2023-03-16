3044c7132eaf7e7c6bc1e907b1b9c0c036a03759
Brenda SC <brenda.saavedra@fondeadora.com>
2023-03-16 13:31:44 -0600

    feat: add macOS target

---
+++ macOS-ChatGPT/macOS_ChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  macOS_ChatGPTApp.swift
+//  macOS-ChatGPT
+//
+//  Created by Brenda Saavedra  on 16/03/23.
+//
+
+import SwiftUI
+
+@main
+struct macOS_ChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
