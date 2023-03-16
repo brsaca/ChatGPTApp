9cb80ed398e913042fde4170e781489d06d37a7a
Brenda SC <brenda.saavedra@fondeadora.com>
2023-03-16 13:29:06 -0600

    feat: add ios targt

---
+++ iOS-ChatGPT/iOS_ChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_ChatGPTApp.swift
+//  iOS-ChatGPT
+//
+//  Created by Brenda Saavedra  on 16/03/23.
+//
+
+import SwiftUI
+
+@main
+struct iOS_ChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
