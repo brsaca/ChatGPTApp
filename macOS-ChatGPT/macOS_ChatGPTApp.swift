09d3fa712147f320d2eb68401b78b25a0b1d6a72
Brenda SC <brenda.saavedra@fondeadora.com>
2023-03-21 16:54:48 -0600

    feat: add CoreManager inside App

--- macOS-ChatGPT/macOS_ChatGPTApp.swift
+++ macOS-ChatGPT/macOS_ChatGPTApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct macOS_ChatGPTApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView().environment(\.managedObjectContext, CoreDataManager.shared.persistentContainer.viewContext)
         }
     }
 }
