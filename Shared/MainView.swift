8fac73fa4f56625d2ef414f84d54eb3ae78fb0f3
Brenda SC <brenda.saavedra@fondeadora.com>
2023-03-21 14:30:24 -0600

    feat: add functionality to paint answers

--- Shared/MainView.swift
+++ Shared/MainView.swift
@@ -6,20 +6,45 @@
 //
 
 import SwiftUI
+import OpenAISwift
 
 struct MainView: View {
     @State private var charText: String = ""
     
+    let openAI = OpenAISwift(authToken: "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")
+    
+    @State private var answers: [String] = []
+    
+    private var isFormValid: Bool {
+        !charText.isEmptyOrWhiteSpace
+    }
+    
+    private func performSearch() {
+        openAI.sendCompletion(with: charText, maxTokens: 500) { result in
+            switch result {
+            case .success(let success):
+                let answer = success.choices.first?.text.trimmingCharacters(in: .whitespacesAndNewlines) ?? ""
+                answers.append(answer)
+            case .failure(let failure):
+                print(failure)
+            }
+        }
+    }
+    
     var body: some View {
         VStack{
+            List(answers, id: \.self) { answer in
+                Text(answer)
+            }
             Spacer()
             HStack{
                 TextField("Search...", text: $charText).textFieldStyle(.roundedBorder)
                 Button{
                     //action
+                    performSearch()
                 } label: {
                     Image(systemName: "paperplane.circle.fill").font(.title).rotationEffect(Angle(degrees: 45))
-                }.buttonStyle(.borderless).tint(.blue)
+                }.buttonStyle(.borderless).tint(.blue).disabled(!isFormValid)
             }
         }.padding(10)
     }
