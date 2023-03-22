34204a4e2113b278c13c89f063be47f3a36d855d
Brenda SC <brenda.saavedra@fondeadora.com>
2023-03-22 17:29:03 -0600

    feat: show navigationSplitView

--- macOS-ChatGPT/ContentView.swift
+++ macOS-ChatGPT/ContentView.swift
@@ -9,13 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationSplitView {
+            HistoryView()
+        } detail: {
+            MainView()
         }
-        .padding()
     }
 }
 
