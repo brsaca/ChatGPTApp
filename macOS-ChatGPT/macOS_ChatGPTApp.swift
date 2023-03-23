0bce07deb67d07ade2561236fd5c7c9ab044e314
Brenda SC <brenda.saavedra@fondeadora.com>
2023-03-22 18:43:57 -0600

    feat: show the app in the toolbar

--- macOS-ChatGPT/macOS_ChatGPTApp.swift
+++ macOS-ChatGPT/macOS_ChatGPTApp.swift
@@ -9,11 +9,48 @@ import SwiftUI
 
 @main
 struct macOS_ChatGPTApp: App {
+    
+    @NSApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(Model())
                 .environment(\.managedObjectContext, CoreDataManager.shared.persistentContainer.viewContext)
+        }
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    private var statusItem: NSStatusItem!
+    private var popover: NSPopover!
+    
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        
+        let contentView = ContentView()
+            .environmentObject(Model())
+            .environment(\.managedObjectContext, CoreDataManager.shared.persistentContainer.viewContext)
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        
+        if let statusButton = statusItem.button {
+            statusButton.image = NSImage(systemSymbolName: "brain", accessibilityDescription: "Brain")
+            statusButton.action = #selector(togglePopover)
+        }
+        
+        self.popover = NSPopover()
+        self.popover.contentSize = NSSize(width: 600, height: 600)
+        self.popover.behavior = .transient
+        self.popover.contentViewController = NSHostingController(rootView: contentView)
+    }
+    
+    @objc func togglePopover() {
+        if let button = statusItem.button {
+            if popover.isShown {
+                self.popover.performClose(nil)
+            }else{
+                self.popover.show(relativeTo: button.bounds, of: button, preferredEdge: .minY)
+            }
         }
     }
 }
